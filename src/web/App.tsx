041cf8fede78065ac70461d8a1dd85d9ab3eaed4
teavver <jerzyx11@gmail.com>
2024-07-25 05:20:44 +0200

    web

---
+++ src/web/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="w-screen h-screen m-0 p-0 bg-neutral-800 text-white font-mono">
+            <p className="">salt-explorer</p>
+        </div>
+    )
+}
+
+export default App
